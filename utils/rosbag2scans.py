25dba07e187c50a4c2eb1158c0adbba49c648e07
phdh4 <2261761989@qq.com>
2025-01-16 15:32:36 +0800

    小修改

--- utils/rosbag2scans.py
+++ utils/rosbag2scans.py
@@ -13,10 +13,11 @@ def point_list_to_cloud(pts_list):
     return cloud
 
 if __name__=="__main__":
-    bag = rosbag.Bag("G:/6_2024-12-21-11-03-17_75.bag")
+    bag = rosbag.Bag("E:/hzy/data/depth_predict/6_2024-12-21-11-01-13_74.bag")
     counter = 0
     for topic, msg, t in bag.read_messages(topics=['/benewakeHornX2_lidar_visualization_A']):
         cloud = sensor_msgs.point_cloud2.read_points_list(msg)
         cloud = point_list_to_cloud(cloud)
         np.save(os.path.join("./", str(counter) + '.npy'), cloud)
         counter += 1
+
